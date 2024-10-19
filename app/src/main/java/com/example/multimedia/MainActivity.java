1e01060e0242c676dcbca951c366c0b9a19beecd
Josemiguel <j.cerda41@alumnos.santotomas.cl>
2024-10-19 15:46:48 -0300

    Activdad multimeadia de android studio
    -Se utilizo un boton para poder reproducir un audio o tambien un video
    
    -Animacion para hacer aparecer una imagen
    
    -Hubo confusiones al principios y se subian las imagenes como archivos xml (eso pasa cuando el Layout principal no se cambia a RelativeLayout)

---
+++ app/src/main/java/com/example/multimedia/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.multimedia;
+
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermissions();
+
+        Button playAudioButton = findViewById(R.id.audioReproducir);
+        playAudioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.audio);
+                mediaPlayer.start();
+            }
+        });
+
+        VideoView videoView = findViewById(R.id.myVideoView);
+        Button playVideoButton = findViewById(R.id.videoVer);
+        playVideoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
+                videoView.setVideoURI(videoUri);
+                videoView.start();
+            }
+        });
+
+        Button animateButton = findViewById(R.id.animateButton);
+        final ImageView imageView = findViewById(R.id.myImageView);
+        animateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation fadeInAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+                imageView.setVisibility(View.VISIBLE);
+                imageView.startAnimation(fadeInAnimation);
+            }
+        });
+
+    }
+
+    private void checkPermissions(){
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
